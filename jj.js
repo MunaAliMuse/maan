cb8e4b3e875c016717aa0a4d1d888ee2970b3b6f
MunaAliMuse <xijaabo143@gmail.com>
2023-11-21 09:46:33 +0300

    wa jitaabo

---
+++ jj.js
@@ -0,0 +1,92 @@
+// for (let num = 1; num<=  5; num++)
+//     console.log(num)
+
+
+// for (let num = 1; num<=  5; num++){
+//     console.log(num)
+
+
+// let name = prompt("enter you name")
+// for  (let num = 1; num <= 15; num++){
+//     document.write(num, " ",name, "<br>" )
+
+
+// }
+
+
+// let Number =  1;
+
+// do{
+//     console.log(Number)
+//     Number++
+//     console.log(Number)
+// }
+// while(Number <= 5)
+
+
+
+// let name =  prompt("enter your name")
+// let age =  prompt("enter your age")
+// let num= 0
+
+// do{
+//     document.write(" my is ",name," my age is ",age, "<br>")
+//     num++
+
+// }
+// while (num <= 5)
+
+
+
+
+// let Number =  5;
+
+// do{
+//     console.log(Number)
+//     document.write(Number, "<br>")
+
+//     Number +=5
+// }
+// while(Number <= 40)
+// document.write(Number, "<br>")
+
+
+// function Addition( ){
+//   let num1=2
+//   let num2 = 8  
+//  let Sum = num1+num2 ;
+
+// document.write(Sum)
+// }
+// Addition()
+
+
+
+
+// function myName (name, age){
+//     console.log ("hello",name,"you are  years old",age)
+// }
+// myName("ali",24)
+// myName("juweyriyo",60)
+// myName("farhiyo",80)
+
+
+
+
+
+function Addition  (num1, num2, num3){
+ 
+    if(num1>num2 &&num1>num3)
+    console.log(num1)
+
+    else if (num2>num1 &&  num2>num3)
+    console.log(num2)
+    
+    else if (num3>num1 && num3>num2)
+    console.log(num3)
+  
+else{
+    console.log("unknown number")
+}
+}
+Addition( 2,2,2)
---
+++ main.js
@@ -0,0 +1,15 @@
+
+// let num  = 2
+// while(num <=20){
+
+//     console.log(num);
+//      num += 2; 
+// }
+
+
+let num  = 1
+while(num <=19){
+    
+    console.log(num);
+     num += 1; 
+}
